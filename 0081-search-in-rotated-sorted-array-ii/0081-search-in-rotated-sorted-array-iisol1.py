f6ec89a64929cfeaa8a09f1220ce87f1bffd9b42
Praneeth kumar <83230376+Praneethkumar57@users.noreply.github.com>
2024-05-23 22:35:59 +0530

    Time: 53 ms (51.81%), Space: 17.1 MB (56.41%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-iisol1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for val in nums:
+            if val == target:
+                return True
+        return False
+        
