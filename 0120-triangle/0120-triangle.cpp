f2c9ddf322282ae8202fcb146239c9bf9505ce20
Praneeth kumar <83230376+Praneethkumar57@users.noreply.github.com>
2025-08-14 23:03:18 +0530

    Time: 0 ms (100%), Space: 13.5 MB (11.89%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,22 @@
 int fn(vector<vector<int>>& triangle,int n,vector<vector<int>>& dp,int row,int col){
-    if(row == n-1){
+    if(row==n-1){
         return(triangle[row][col]);
     }
-
-    if(dp[row][col]!=-1e8){
-        return(dp[row][col]);
+    if(dp[row][col]!=1e9){
+        return dp[row][col];
     }
-    
-    int down = triangle[row][col] +fn(triangle,n,dp,row+1,col);
-    int diagonal = triangle[row][col] +fn(triangle,n,dp,row+1,col+1);
 
-    return(dp[row][col] = min(down,diagonal));
+    int down = triangle[row][col] +fn(triangle,n,dp,row+1,col);
+    int diagonal = triangle[row][col] + fn(triangle,n,dp,row+1,col+1);
+    dp[row][col] = min(down,diagonal);
+    return(dp[row][col]);
 }
 
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1e8));
+        int n= triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
         return(fn(triangle,n,dp,0,0));
     }
 };
